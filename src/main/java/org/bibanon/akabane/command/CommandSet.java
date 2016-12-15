f5f476fdc25404dce3ddff34a429800c4dfa52ff
SpeaKeasY <speakeasysky@gmail.com>
2016-12-15 16:48:56 +0000

    Reflection to call command methods.

--- src/main/java/org/bibanon/akabane/command/CommandSet.java
+++ src/main/java/org/bibanon/akabane/command/CommandSet.java
@@ -5,7 +5,11 @@
  */
 package org.bibanon.akabane.command;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.pircbotx.hooks.events.MessageEvent;
 
 /**
@@ -20,7 +24,36 @@ public class CommandSet extends Command {
 
     @Override
     public void process(String[] message, MessageEvent event) {
-        ;
+
+        if (message.length > 1) {
+            Method method;
+            for (int i = 0; i < message.length; i++) {
+                for (String arg : commandArgsNames.keySet()) {
+                    if (message[i] == arg) {
+                        if (commandArgsNames.get(arg)) {
+                            try {
+                                // command takes arg
+                                method = commandClass.getDeclaredMethod(arg, String.class);
+                                i++;
+                                method.invoke(commandClass, message[i]);
+                            } catch (NoSuchMethodException ex) {
+                                Logger.getLogger(CommandGrab.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (SecurityException ex) {
+                                Logger.getLogger(CommandGrab.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (IllegalAccessException ex) {
+                                Logger.getLogger(CommandGrab.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (IllegalArgumentException ex) {
+                                Logger.getLogger(CommandGrab.class.getName()).log(Level.SEVERE, null, ex);
+                            } catch (InvocationTargetException ex) {
+                                Logger.getLogger(CommandGrab.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            event.respond("HELP: Usage: [not implemented]");
+        }
     }
 
 }
